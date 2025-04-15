73cbcd2193c313ae1817b280b818b7b507d857a2
Ross Duquette <ross.duquette@gastronomous.ca>
2025-04-15 16:28:23 -0700

    Make loop for listener consistent with speaker

--- listener.py
+++ listener.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from client import create_client
 from subscriber import Subscriber
 
@@ -7,7 +9,10 @@ def test_callback(msg):
 def main():
     client = create_client()
     sub = Subscriber(client, "test", test_callback)
-    client.loop_forever()
+    client.loop_start()
+    while True:
+        sleep(1)
+    client.loop_end()
 
 if __name__ == "__main__":
     main()
